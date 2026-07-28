9689323984fe1a6fa84a173d6fda72836fea20e8
rahulltelkar <rahul.friends262@gmail.com>
2026-07-28 03:48:08 +0000

    Add iam.tf file

---
+++ terraform/iam.tf
@@ -0,0 +1,71 @@
+resource "aws_iam_role" "eks_cluster" {
+  name = "${var.environment}-eks-cluster-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [
+      {
+        Effect = "Allow"
+
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
+
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "${var.environment}-eks-cluster-role"
+    }
+  )
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
+  role       = aws_iam_role.eks_cluster.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+resource "aws_iam_role" "worker_nodes" {
+  name = "${var.environment}-worker-node-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [
+      {
+        Effect = "Allow"
+
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "${var.environment}-worker-node-role"
+    }
+  )
+}
+
+resource "aws_iam_role_policy_attachment" "worker_node" {
+  role       = aws_iam_role.worker_nodes.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
+resource "aws_iam_role_policy_attachment" "worker_ecr" {
+  role       = aws_iam_role.worker_nodes.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+resource "aws_iam_role_policy_attachment" "worker_cni" {
+  role       = aws_iam_role.worker_nodes.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+}
