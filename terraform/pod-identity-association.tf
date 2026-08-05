40e225c059e2e471a27f4d498a43ec7b71e44527
rahulltelkar <rahul.friends262@gmail.com>
2026-08-05 06:33:50 +0000

    create eks pod identity addons resource

--- terraform/pod-identity-association.tf
+++ terraform/pod-identity-association.tf
@@ -26,9 +26,21 @@
 # the associated IAM Role.
 ##############################################################
 
+# Install EKS Pod Identity Agent
+
+resource "aws_eks_addon" "pod_identity_agent" {
+  cluster_name = aws_eks_cluster.main.name
+  addon_name   = "eks-pod-identity-agent"
+}
+# Associate ServiceAccount with IAM Role
+
 resource "aws_eks_pod_identity_association" "alb_controller" {
   cluster_name    = aws_eks_cluster.main.name
   namespace       = "kube-system"
   service_account = "aws-load-balancer-controller"
   role_arn        = aws_iam_role.alb_controller.arn
+
+  depends_on = [
+    aws_eks_addon.pod_identity_agent
+  ]
 }
